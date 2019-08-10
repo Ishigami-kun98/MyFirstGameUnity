e5229a5974f84f887e6cecb6a625c4bf811aafa1
Ishigami-kun98 <48060336+Ishigami-kun98@users.noreply.github.com>
2019-08-10 22:06:57 +0200

    Add files via upload

---
+++ Assets/Script/StartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
